6a247aa80a902dd99a6b127214627054d153b26d
Pravin Rathod <pravinrathod206@gmail.com>
2023-12-26 17:17:09 +0400

    rewrote upvote and comment endpoint

--- src/server.js
+++ src/server.js
@@ -1,26 +1,41 @@
 import express from "express";
+import { MongoClient } from "mongodb";
 //creates express app
 const app = express();
 const port = 8000;
 
-console.log("hey");
+// for parsing application/json
+app.use(express.json());
 
-app.use(express.json()); // for parsing application/json
-
-//this is fake data
-const articlesInfo = [
-    { name: "learn-react", upvotes: 0, comments: [] },
-    { name: "learn-node", upvotes: 0, comments: [] },
-    { name: "learn-mongodb", upvotes: 0, comments: [] },
-];
-
-//route for upvoting articles
-app.put("/api/articles/:name/upvote", (req, res) => {
+//get route to fetch article
+app.get("/api/articles/:name", async function (req, res) {
     const { name } = req.params;
-    console.log(name);
-    const article = articlesInfo.find((article) => article.name === name);
+    const client = new MongoClient("mongodb://127.0.0.1:27017");
+    await client.connect();
+    const db = client.db("react-blog-db");
+    const article = await db.collection("articles").findOne({ name });
     if (article) {
-        article.upvotes += 1;
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+//put route for upvoting articles
+app.put("/api/articles/:name/upvote", async (req, res) => {
+    const { name } = req.params;
+    const client = new MongoClient("mongodb://127.0.0.1:27017");
+    await client.connect();
+    const db = client.db("react-blog-db");
+
+    await db.collection("articles").updateOne(
+        { name },
+        {
+            $inc: { upvotes: 1 },
+        }
+    );
+    const article = await db.collection("articles").findOne({ name });
+    if (article) {
         res.send(
             `The ${article.name} article has ${article.upvotes} upvotes now`
         );
@@ -30,13 +45,21 @@ app.put("/api/articles/:name/upvote", (req, res) => {
 });
 
 //route for commenting articles
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
     const { postedBy, text } = req.body;
     const { name } = req.params;
-    console.log(name);
-    const article = articlesInfo.find((article) => article.name === name);
+    const client = new MongoClient("mongodb://127.0.0.1:27017");
+    await client.connect();
+    const db = client.db("react-blog-db");
+
+    await db.collection("articles").updateOne(
+        { name },
+        {
+            $push: { comments: { postedBy, text } },
+        }
+    );
+    const article = await db.collection("articles").findOne({ name });
     if (article) {
-        article.comments.push({ postedBy, text });
         res.send(article.comments);
     } else {
         res.send(`That article doesn\'t exist`);
